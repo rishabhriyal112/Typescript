dbac2a935e6800871c6800c802a6d66cb7a1a4aa
rishabhriyal112 <riyalrishabh22@gmail.com>
2026-09-14 18:33:00 +0530

    Typescript basic code and understand type inference vs type annotation

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(name : string) :string{
+    return `Hello ${name}, Welcome To the Typescript!!`
+}
+
+const username :string = "Rishabh"
+console.log(greet(username))
---
+++ src/typesInTs.ts
@@ -0,0 +1,7 @@
+//Type Inference - 
+let age = 25
+// age = "hello" shows error
+
+//Type Annotation
+let age2 : number = 25
+//age2 = "hello" type already defined so you can't reassign another type variable 
